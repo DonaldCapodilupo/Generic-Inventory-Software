15c1be19a66171f8a5c0a121edb0f643aa3452c6
DonaldCapodilupo <Donald@Capodilupo.dev>
2021-07-31 09:20:42 -0400

    Login screen now sucessfully routs to main when a correct username and password combination is found.

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=["POST","GET"])
 def login_Screen():
-    return render_template("login.html")
+    if request.method == 'POST':
+        from backend import get_Current_User_Database_Information
+        username = request.form['username']
+        password = request.form['password']
+
+        valid_Users = get_Current_User_Database_Information()
+
+        for user, pwrd in valid_Users.items():
+            if user == username and password == pwrd:
+                return render_template("main.html")
+        return render_template("login.html")
+
+
+    else:
+        return render_template("login.html")
 
 
 if __name__ == '__main__':
