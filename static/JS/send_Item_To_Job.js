45dd50099694655ed31fd492c57de7bf0c6ed069
DonaldCapodilupo <Donald@Capodilupo.dev>
2021-08-04 05:58:24 -0400

    "send to job" utilizes javascript to display warnings and reveal apropriate div tags. Need to incorporate backend and add some finishing touches.

--- static/JS/send_Item_To_Job.js
+++ static/JS/send_Item_To_Job.js
@@ -1,18 +1,41 @@
-
-
 function check_Invoice_Type(){
     const previous_Invoice_Choice = document.getElementById("Previous Invoice");
     const new_Invoice_Choice = document.getElementById("New Invoice");
-    const new_Invoice_Number = document.getElementById("New Invoice Number")
+    const new_Invoice_Number = document.getElementById("New Invoice Number");
 
 
 
-    if(new_Invoice_Choice.checked === true){
-        alert("Invoice number "+ new_Invoice_Number.value + " has been added to the database.")
+    if(new_Invoice_Choice.checked === true && new_Invoice_Number.value){
+        document.getElementById("New Contractor Wrapper").style.display = "block";
+        document.getElementById("Final Choice Wrapper").style.display = "none";
     }
 
-    else if (previous_Invoice_Choice.checked === true){
-        alert("Good to go.")
+    else if (new_Invoice_Choice.checked === true && new_Invoice_Number.value === ""){
+        alert("You cannot leave the invoice number field blank.");
+
+    }
+
+    else if(previous_Invoice_Choice.checked === true){
+        document.getElementById("New Contractor Wrapper").style.display = "none";
+        document.getElementById("Final Choice Wrapper").style.display = "block";
+    }
+
+}
+
+function verify_Contractor(){
+    const previous_Contractor_Choice = document.getElementById("Previous Contractor");
+    const new_Contractor_Choice = document.getElementById("New Contractor");
+    const new_Contractor_Name = document.getElementById("Contractor Name");
+
+
+
+    if(new_Contractor_Choice.checked === true && new_Contractor_Name.value){
+        document.getElementById("Final Choice Wrapper").style.display = "block";
+
+    }
+
+    else if (new_Contractor_Choice.checked === true && new_Contractor_Name.value === ""){
+        alert("You need to fill in the \"New Contractor Name\" field.");
 
     }
 
